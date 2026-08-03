ad98601aaa567e2857b4c9a6f4d0201580e15ecb
EK-399 <eugeniekwok123@gmail.com>
2026-08-03 14:59:32 +1200

    Traps, Checkpoints, Animation

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    Vector3 startPosition;
+    public Transform recentCheckpoint;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Death"))
+        {
+            Die();
+        }
+
+        if (collision.CompareTag("Checkpoint"))
+        {
+            recentCheckpoint = collision.gameObject.transform;
+            collision.gameObject.GetComponent<Animator>().Play("hit");
+        }
+    }
+
+    public void Die()
+    {
+        if (recentCheckpoint == null)
+        {
+            transform.position = startPosition; //
+        }
+        else
+        {
+            transform.position = recentCheckpoint.position;
+        }
+
+  
+    }
+}
